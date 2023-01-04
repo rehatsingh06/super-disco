8f57f7ebfcdb096f1a3e887fcf373c84688f7115
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-04 14:26:59 +0400

    digital audio devices

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import gpiozero
+import playsound
+
+
+a_major_input = gpiozero.DigitalInputDevice(17)
+b_major_input = gpiozero.DigitalInputDevice(17)
+c_major_input = gpiozero.DigitalInputDevice(17)
+d_major_input = gpiozero.DigitalInputDevice(17)
+e_major_input = gpiozero.DigitalInputDevice(17)
+f_major_input = gpiozero.DigitalInputDevice(17)
+g_major_input = gpiozero.DigitalInputDevice(17)
+
+a_major_input.when_activated = playsound.playsound(
+    "notes/A_major", block=False
+)
+b_major_input.when_activated = playsound.playsound(
+    "notes/B_major", block=False
+)
+c_major_input.when_activated = playsound.playsound(
+    "notes/C_major", block=False
+)
+d_major_input.when_activated = playsound.playsound(
+    "notes/D_major", block=False
+)
+e_major_input.when_activated = playsound.playsound(
+    "notes/E_major", block=False
+)
+f_major_input.when_activated = playsound.playsound(
+    "notes/F_major", block=False
+)
+g_major_input.when_activated = playsound.playsound(
+    "notes/G_major", block=False
+)
 
 
 def main():
-    print("Hello world")
+    # test
+    playsound.playsound("notes/A_major")
 
 
 if __name__ == "__main__":
