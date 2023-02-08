58283e082b3a369772fe06d0e29edb9e4f46b894
Rehatbir Singh <113099975+rehatsingh06@users.noreply.github.com>
2023-02-08 13:30:18 +0400

    added sharp sounds

--- main.py
+++ main.py
@@ -6,21 +6,50 @@ mixer.init()
 # Set this variable to play a sound
 SOUND: mixer.Channel | None = None
 
+# TODO: Change pins
+# INPUTS
+
 a_major_input = gpiozero.DigitalInputDevice(17)
+a_sharp_input = gpiozero.DigitalInputDevice(17)
+
 b_major_input = gpiozero.DigitalInputDevice(18)
+
 c_major_input = gpiozero.DigitalInputDevice(19)
+c_sharp_input = gpiozero.DigitalInputDevice(19)
+
 d_major_input = gpiozero.DigitalInputDevice(20)
+d_sharp_input = gpiozero.DigitalInputDevice(20)
+
 e_major_input = gpiozero.DigitalInputDevice(21)
+
 f_major_input = gpiozero.DigitalInputDevice(22)
+f_sharp_input = gpiozero.DigitalInputDevice(22)
+
 g_major_input = gpiozero.DigitalInputDevice(23)
+g_sharp_input = gpiozero.DigitalInputDevice(23)
+
+
+# SOUNDS
 
 a_major = mixer.Sound("notes/A_major.wav")
+a_sharp = mixer.Sound("notes/A_sharp.wav")
+
 b_major = mixer.Sound("notes/B_major.wav")
+
 c_major = mixer.Sound("notes/C_major.wav")
+c_sharp = mixer.Sound("notes/C_sharp.wav")
+
 d_major = mixer.Sound("notes/D_major.wav")
+d_sharp = mixer.Sound("notes/D_sharp.wav")
+
 e_major = mixer.Sound("notes/E_major.wav")
+
 f_major = mixer.Sound("notes/F_major.wav")
+f_sharp = mixer.Sound("notes/F_sharp.wav")
+
 g_major = mixer.Sound("notes/G_major.wav")
+g_sharp = mixer.Sound("notes/G_sharp.wav")
+
 
 a_major_input.when_activated = a_major.play
 b_major_input.when_activated = b_major.play
