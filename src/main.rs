568f27f68a8754809eedbf83d25ad918cebe90ab
Rehatbir Singh <rehatbir.singh@gmail.com>
2023-02-23 22:45:37 +0400

    better

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ use rppal::{gpio::Gpio, system::DeviceInfo};
 
 fn load_sound_source(file_path: &str) -> Result<Decoder<BufReader<File>>, Box<dyn Error>> {
     let file = BufReader::new(
-        File::open(file_path)
-            .map_err(|e| format!("Unable to open file '{}': {}", file_path, e))?,
+        File::open(file_path).map_err(|e| format!("Unable to open file '{}': {}", file_path, e))?,
     );
     Decoder::new(file).map_err(|e| e.into())
 }
@@ -17,19 +16,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let device_info = DeviceInfo::new()?;
     println!("Device: {:?}\nStarting piano...", device_info);
 
-    let a_major_sound = load_sound_source("notes/A_major.wav").unwrap();
-    let b_major_sound = load_sound_source("notes/B_major.wav").unwrap();
-    let c_major_sound = load_sound_source("notes/C_major.wav").unwrap();
-    let d_major_sound = load_sound_source("notes/D_major.wav").unwrap();
-    let e_major_sound = load_sound_source("notes/E_major.wav").unwrap();
-    let f_major_sound = load_sound_source("notes/F_major.wav").unwrap();
-    let g_major_sound = load_sound_source("notes/G_major.wav").unwrap();
+    let a_major_sound = load_sound_source("notes/A_major.wav")?;
+    let b_major_sound = load_sound_source("notes/B_major.wav")?;
+    let c_major_sound = load_sound_source("notes/C_major.wav")?;
+    let d_major_sound = load_sound_source("notes/D_major.wav")?;
+    let e_major_sound = load_sound_source("notes/E_major.wav")?;
+    let f_major_sound = load_sound_source("notes/F_major.wav")?;
+    let g_major_sound = load_sound_source("notes/G_major.wav")?;
 
-    let a_sharp_sound = load_sound_source("notes/A_sharp.wav").unwrap();
-    let c_sharp_sound = load_sound_source("notes/C_sharp.wav").unwrap();
-    let d_sharp_sound = load_sound_source("notes/D_sharp.wav").unwrap();
-    let f_sharp_sound = load_sound_source("notes/F_sharp.wav").unwrap();
-    let g_sharp_sound = load_sound_source("notes/G_sharp.wav").unwrap();
+    let a_sharp_sound = load_sound_source("notes/A_sharp.wav")?;
+    let c_sharp_sound = load_sound_source("notes/C_sharp.wav")?;
+    let d_sharp_sound = load_sound_source("notes/D_sharp.wav")?;
+    let f_sharp_sound = load_sound_source("notes/F_sharp.wav")?;
+    let g_sharp_sound = load_sound_source("notes/G_sharp.wav")?;
 
     Ok(())
 }
