74cd7eb1781eb9fa043e7ec3f3df7134896d8e07
Rehatbir Singh <rehatbir.singh@gmail.com>
2023-11-27 17:04:20 +0400

    fixed stuff

--- main.py
+++ main.py
@@ -13,24 +13,18 @@ SOUND: mixer.Channel | None = None
 # TODO: Change pins
 # INPUTS
 
-a_major_input = gpiozero.Button(14)
-a_sharp_input = gpiozero.Button(17)
-
-b_major_input = gpiozero.Button(15)
-
-c_major_input = gpiozero.Button(18)
-c_sharp_input = gpiozero.Button(27)
-
-d_major_input = gpiozero.Button(23)
-d_sharp_input = gpiozero.Button(22)
-
-e_major_input = gpiozero.Button(24)
-
-f_major_input = gpiozero.Button(25)
-f_sharp_input = gpiozero.Button(10)
-
-g_major_input = gpiozero.Button(8)
-g_sharp_input = gpiozero.Button(9)
+a_major_input = gpiozero.DigitalInputDevice(14, bounce_time=0.1)
+a_sharp_input = gpiozero.DigitalInputDevice(17, bounce_time=0.1)
+b_major_input = gpiozero.DigitalInputDevice(15, bounce_time=0.1)
+c_major_input = gpiozero.DigitalInputDevice(18, bounce_time=0.1)
+c_sharp_input = gpiozero.DigitalInputDevice(27, bounce_time=0.1)
+d_major_input = gpiozero.DigitalInputDevice(23, bounce_time=0.1)
+d_sharp_input = gpiozero.DigitalInputDevice(22, bounce_time=0.1)
+e_major_input = gpiozero.DigitalInputDevice(24, bounce_time=0.1)
+f_major_input = gpiozero.DigitalInputDevice(25, bounce_time=0.1)
+f_sharp_input = gpiozero.DigitalInputDevice(10, bounce_time=0.1)
+g_major_input = gpiozero.DigitalInputDevice(8, bounce_time=0.1)
+g_sharp_input = gpiozero.DigitalInputDevice(9, bounce_time=0.1)
 
 
 # SOUNDS
@@ -55,33 +49,33 @@ g_major = mixer.Sound(NOTES_DIR / "G_major.wav")
 g_sharp = mixer.Sound(NOTES_DIR / "G_sharp.wav")
 
 
-def play_sound(sound: mixer.Sound) -> Callable[[], None]:
+def play_sound(sound: mixer.Sound, note: str) -> Callable[[], None]:
     def wrapped():
         sound.play()
         mixer.fadeout(4000)
-        #print("Playing sound")
+        print("Playing note", note)
 
     return wrapped
 
 
-a_major_input.when_activated = play_sound(a_major)
-a_sharp_input.when_activated = play_sound(a_sharp)
+a_major_input.when_activated = play_sound(a_major, "A")
+a_sharp_input.when_activated = play_sound(a_sharp, "A#")
 
-b_major_input.when_activated = play_sound(b_major)
+b_major_input.when_activated = play_sound(b_major, "B")
 
-c_major_input.when_activated = play_sound(c_major)
-c_sharp_input.when_activated = play_sound(c_sharp)
+c_major_input.when_activated = play_sound(c_major, "C")
+c_sharp_input.when_activated = play_sound(c_sharp, "C#")
 
-d_major_input.when_activated = play_sound(d_major)
-d_sharp_input.when_activated = play_sound(d_sharp)
+d_major_input.when_activated = play_sound(d_major, "D")
+d_sharp_input.when_activated = play_sound(d_sharp, "D#")
 
-e_major_input.when_activated = play_sound(e_major)
+e_major_input.when_activated = play_sound(e_major, "E")
 
-f_major_input.when_activated = play_sound(f_major)
-f_sharp_input.when_activated = play_sound(f_sharp)
+f_major_input.when_activated = play_sound(f_major, "F")
+f_sharp_input.when_activated = play_sound(f_sharp, "F#")
 
-g_major_input.when_activated = play_sound(g_major)
-g_sharp_input.when_activated = play_sound(g_sharp)
+g_major_input.when_activated = play_sound(g_major, "G")
+g_sharp_input.when_activated = play_sound(g_sharp, "G#")
 
 
 def main():
